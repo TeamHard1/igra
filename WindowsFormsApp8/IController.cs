6a89a488ed89db8bf3dd505379e4bf3941383c3d
user <user@C1011>
2022-04-01 12:05:01 +0300

    Делигаты

--- WindowsFormsApp8/IController.cs
+++ WindowsFormsApp8/IController.cs
@@ -8,5 +8,6 @@ namespace WindowsFormsApp8
 {
     internal interface IController
     {
+
     }
 }
---
+++ WindowsFormsApp8/Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp8
+{
+    internal class Model
+    {
+        int[,] array = new int[5, 7];
+        private void InEntry(int i, int j)
+        {
+            array[i, j] = 1;
+        }
+
+        delegate void Delegate(int i, double j);
+
+    }
+    
+}
