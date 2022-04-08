575563b4236a9b0e68be177c972effe8c14444e9
bekingalieva2015 <bekingalieva2015@yandex.ru>
2022-04-08 11:20:05 +0300

    12

--- WindowsFormsApp8/Form1.cs
+++ WindowsFormsApp8/Form1.cs
@@ -8,16 +8,33 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace WindowsFormsApp8
 {
     public partial class Form1 : Form, IView
     {
         private IView view;
         private IController controller;
+        Button[,] button;
+
+
+
+        
+       //123456789
         public Form1()
         {
             InitializeComponent();
+            button = new Button[7, 5];
 
+            for (int i = 0; i < 7; i++)
+                for (int j = 0; j < 5; j++)
+                {
+                    button[i, j] = new Button();
+                    button[i, j].Parent = dataGridView1; 
+                    button[i, j].Location = new Point(5 + 70 * i, 5 + 70 * j);
+                    button[i, j].Size = new Size(70, 70); 
+                    button[i, j].FlatAppearance.BorderSize = 0;               
+                }
 
         }
 
@@ -38,9 +55,8 @@ namespace WindowsFormsApp8
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            dataGridView1.ColumnCount = 5;
-            dataGridView1.RowCount = 7;
-           
+            
         }
-    }
+		
+	}
 }
